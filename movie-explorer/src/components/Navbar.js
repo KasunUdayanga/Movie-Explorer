f3100950767d55492f9e13c886afbf40b19d09dc
Kasun_Udayanga <udayangakasun696@gmail.com>
2025-05-08 20:00:55 +0530

    style: refine Navbar component by optimizing class names for consistency and transition effects

--- movie-explorer/src/components/Navbar.js
+++ movie-explorer/src/components/Navbar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Navbar = () => {
   return (
-    <nav className="bg-blue-800 text-gray-100  shadow-lg">
+    <nav className="bg-blue-800 text-gray-100 shadow-lg">
       <div className="container mx-auto flex justify-between items-center p-4">
         {/* Logo */}
-        <Link to="/" className="text-2xl font-bold  hover:text-blue-400 transition">
+        <Link to="/" className="text-2xl font-bold hover:text-blue-400 transition duration-300">
           Movie Explorer
         </Link>
 
@@ -15,18 +14,16 @@ const Navbar = () => {
         <div className="flex items-center space-x-6">
           <Link
             to="/"
-            className="text-lg hover:text-blue-400 transition"
+            className="text-lg hover:text-blue-400 transition duration-300 transform hover:scale-110"
           >
             Home
           </Link>
           <Link
             to="/favorites"
-            className="text-lg hover:text-blue-400 transition"
+            className="text-lg hover:text-blue-400 transition duration-300 transform hover:scale-110"
           >
             Favorites
           </Link>
-      
-
         </div>
       </div>
     </nav>
